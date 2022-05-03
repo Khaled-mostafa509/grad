9fe7d5957fac2b9ac4b39a7e9c0e3d8549bc52a0
khaled-mostafa509 <khaledmostafa509@gmail.com>
2022-05-03 10:05:13 +0200

    make it better5

--- test_project/urls.py
+++ test_project/urls.py
@@ -19,9 +19,9 @@ from django.urls import path ,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('rest-auth/', include('rest_auth.urls')),
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api/', include('authentications.urls', namespace='api')),
     
 ]
