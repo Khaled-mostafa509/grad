2e2309122a69b860bc16654c370b045a2dbebe5b
khaled-mostafa509 <khaledmostafa509@gmail.com>
2022-05-07 09:12:41 +0200

    make it better13

--- authentications/views.py
+++ authentications/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth import login
 
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.views import LoginView as KnoxLoginView
 
 # class PersonRegistrationView(RegisterView):
 #     serializer_class = PersonCustomRegistrationSerializer
