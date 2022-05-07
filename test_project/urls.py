1058fac9919948206867b2e941fa821686d76a1a
khaled-mostafa509 <khaledmostafa509@gmail.com>
2022-05-07 08:50:00 +0200

    make it better7

--- test_project/urls.py
+++ test_project/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
     # path('rest-auth/', include('rest_auth.urls')),
     # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('api/', include('authentications.urls', namespace='api')),
+    # path('api/', include('authentications.urls', namespace='api')),
     
 ]
 
