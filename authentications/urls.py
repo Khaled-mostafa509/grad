c88c706e5c0b7aa22caa5ea6ac281af12ddfbe5f
khaled-mostafa509 <khaledmostafa509@gmail.com>
2022-05-07 07:07:21 +0200

    make it better2

--- authentications/urls.py
+++ authentications/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import PersonRegistrationView,CompanyRegistrationView,LoginAPI
+# from .views import PersonRegistrationView,CompanyRegistrationView,LoginAPI
 
 app_name = 'authentications'
 
 urlpatterns = [
     
-    path('registration/user/', PersonRegistrationView.as_view(), name='register-user'),
-    path('registration/company/', CompanyRegistrationView.as_view(), name='register-company'),
+    # path('registration/user/', PersonRegistrationView.as_view(), name='register-user'),
+    # path('registration/company/', CompanyRegistrationView.as_view(), name='register-company'),
     path('login/',LoginAPI.as_view(),name='login user')
 ]
