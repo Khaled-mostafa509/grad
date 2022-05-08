8df031a6b766d0022933c6567d365a4bb112b0d5
khaled-mostafa509 <khaledmostafa509@gmail.com>
2022-05-08 20:22:02 +0200

    make it better1

--- test_project/urls.py
+++ test_project/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('user/',include('authentications.urls')),
-    path('api-token-auth', views.obtain_auth_token)
+    # path('user/',include('authentications.urls')),
+    # path('api-token-auth', views.obtain_auth_token)
     # path('api-auth/', include('rest_framework.urls')),
     # path('rest-auth/', include('rest_auth.urls')),
     # path('rest-auth/registration/', include('rest_auth.registration.urls')),
