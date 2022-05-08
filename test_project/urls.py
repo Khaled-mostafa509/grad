38a348feeb54aaa4724562cc8549f24e26903e3b
khaled-mostafa509 <khaledmostafa509@gmail.com>
2022-05-08 20:05:22 +0200

    make it better20

--- test_project/urls.py
+++ test_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path ,include
 from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +30,5 @@ urlpatterns = [
     # path('api/', include('authentications.urls')),
     
     
-]
-
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
