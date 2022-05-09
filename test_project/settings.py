494b51f074ad6c5a4550a495ec0e14d15f668bfb
khaled-mostafa509 <khaledmostafa509@gmail.com>
2022-05-09 02:11:58 +0200

    make it better22

--- test_project/settings.py
+++ test_project/settings.py
@@ -33,11 +33,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-# AUTH_USER_MODEL='authentications.User'
-# ACCOUNT_UNIQUE_EMAIL= True
-# ACCOUNT_EMAIL_REQUIRED = True
-# ACCOUNT_AUTHENTICATION_METHOD = 'username'
-# ACCOUNT_EMAIL_VERIFICATION = 'none'# Application definition
+AUTH_USER_MODEL='authentications.User'
+ACCOUNT_UNIQUE_EMAIL= True
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_EMAIL_VERIFICATION = 'none'# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -46,17 +46,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'authentications',
+    'authentications',
     'rest_framework',
     # 'rest_framework.authtoken',
     'home',
     'helpers',
     # 'rest_auth',
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
-    # 'rest_auth.registration',
-    # 'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth.registration',
+    'rest_framework.authtoken',
     # 'knox',
     # 'phonenumber_field',
 ]
